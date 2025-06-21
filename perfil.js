e98edffb837456e1a668ad3f1f0ff7eea1c3cf21
Murilofossa <murilo.fossa@aluno.ifsp.edu.br>
2025-06-21 16:34:32 -0300

    Create perfil.js

---
+++ perfil.js
@@ -0,0 +1,29 @@
+nav{ 
+    display: flex; 
+    justify-content: space-around; 
+    background-color: rgba(187, 1, 1, 0.781); 
+    padding: 40px 10px; 
+} 
+a{ 
+    color: rgb(255, 255, 255); 
+    text-decoration: none;
+}
+img {
+  border-radius: 70%;
+  border-style: solid;
+   width: 400px;
+  height: 400px;
+}
+h1{
+    text-align: center;
+}
+.container{
+    display: flex;
+}
+p{
+      font-size: 30px;
+}
+h2{
+    text-align: center;
+    font-size: 50;
+}
